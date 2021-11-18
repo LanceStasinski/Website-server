7be0d265366e430027599f950f84fa6aeec943e3
LanceStasinski <lance.stasinski@gmail.com>
2021-11-18 14:34:55 -0500

    feat: Add validation to login route

--- src/routes/auth-routes.ts
+++ src/routes/auth-routes.ts
@@ -4,12 +4,20 @@ import { signUp, login } from "../controllers/auth-controllers";
 
 const router = express.Router();
 
-router.post("/signup", [
-  check("username").not().isEmpty(),
-  check("password").isLength({ min: 6 }),
-  check("confirmPassword").isLength({ min: 6 }),
-], signUp);
+router.post(
+  "/signup",
+  [
+    check("username").not().isEmpty(),
+    check("password").isLength({ min: 6 }),
+    check("confirmPassword").isLength({ min: 6 }),
+  ],
+  signUp
+);
 
-router.post("/login", login);
+router.post(
+  "/login",
+  [check("username").not().isEmpty(), check("password").not().isEmpty()],
+  login
+);
 
 export default router;

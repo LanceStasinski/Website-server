1dcb3da17d49733ba9e157a213ab56c12283f970
LanceStasinski <lance.stasinski@gmail.com>
2021-11-29 14:44:32 -0500

    feat: Add postComment controller

--- src/controllers/blog-controller.ts
+++ src/controllers/blog-controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
+import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 
 import HttpError from "../models/http-error";
 import { commentModel as Comment } from "../models/comment";
+import { userModel as User } from "../models/user";
 
 export const postComment = async (
   req: Request,
@@ -14,10 +16,53 @@ export const postComment = async (
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(new HttpError('Please enter a comment before submitting.', 422))
+    const err = new HttpError("Please enter a comment before submitting.", 422);
+    next(err);
+    return err;
   }
 
-  const { userId, comment, postId } = req.body;
+  const { newComment, postId } = req.body;
+  const userId = req.userId;
 
+  const createdComment = new Comment({
+    creator: userId,
+    comment: newComment,
+    postId,
+  });
 
+  let user;
+  try {
+    user = await User.findById(userId);
+  } catch (error) {
+    const err = new HttpError(
+      "Adding comment failed, please try again later.",
+      500
+    );
+    next(err);
+    return err;
+  }
+
+  if (!user) {
+    const err = new HttpError(
+      "This user does not exist. Please login again.",
+      500
+    );
+    next(err);
+    return err;
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdComment.save({ session: sess });
+    user.comments.push(createdComment);
+    await user.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (error) {
+    const err = new HttpError("Could not save comment.", 500);
+    next(err);
+    return err;
+  }
+
+  res.status(201).json({ message: "Comment created" });
 };
--- src/routes/blog-routes.ts
+++ src/routes/blog-routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { check } from "express-validator";
+import { auth } from "../middleware/check-auth";
+import { postComment } from "../controllers/blog-controller";
 
 const router = express.Router();
 
-router.post("/comment", check("newComment").not().isEmpty());
+router.use(auth);
+
+router.post("/comment", check("newComment").not().isEmpty(), postComment);
 
 export default router;

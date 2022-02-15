ada16c404f15e0a2fee9eb1dd3fabe69f27f0955
LanceStasinski <lance.stasinski@gmail.com>
2022-02-15 07:01:07 -0500

    fix: Add new route to build

--- dist/src/routes/landing-page-routes.js
+++ dist/src/routes/landing-page-routes.js
@@ -9,7 +9,6 @@ const rootDir = path_1.default.dirname(require.main.filename);
 const router = express_1.default.Router();
 router.use(express_1.default.static(path_1.default.join(rootDir, "public", "landing-page", "public")));
 router.get('/', (req, res, next) => {
-    console.log(rootDir);
     res.sendFile(path_1.default.resolve(rootDir, "./public/landing-page", "index.html"));
 });
 exports.default = router;
